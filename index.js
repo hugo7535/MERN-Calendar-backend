351b5af5752c2c3d9054add9562965227737ba15
HugoAldrete <quezadahugo753@gmail.com>
2025-01-31 16:19:08 -0600

    Se agrega React App

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { dbConnection } = require('./database/config');
 const cors = require('cors');
+const path = require('path');
 require('dotenv').config();
 
 // Crear el servidor de express
@@ -21,6 +22,10 @@ app.use(express.json());
 // Rutas
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/eventos', require('./routes/events'));
+
+app.use('*', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+});
 
 // Escuchar peticiones
 app.listen(process.env.PORT, () => {
